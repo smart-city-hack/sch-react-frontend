14ee0683bc879d35d80d98cc1f166332d6ce65c0
Prakita Rayyan Renatin <ge25zow@mytum.de>
2021-10-31 00:57:26 +0200

    mid_status

--- utils/machineLearning.js
+++ utils/machineLearning.js
@@ -1,5 +1,9 @@
 import { postUpdate } from "./serverApi";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+
+const threshold = 0.50;
 
 export async function loadModel(model, setModel) {
   let tryCounter = 0;
@@ -7,7 +11,9 @@ export async function loadModel(model, setModel) {
   while (!model && !loaded && tryCounter < 10) {
     tryCounter++;
     try {
-      const model = await cocoSsd.load();
+      let modelUrl = "https://raw.githubusercontent.com/smart-city-hack/sch-react-frontend/main/public/model_test/model.json";
+      //const model = await cocoSsd.load();
+      const model = await loadGraphModel(modelUrl);
       setModel(model);
       loaded = true;
       console.info("setloadedModel");
@@ -21,7 +27,15 @@ export async function loadModel(model, setModel) {
 export async function predictionFunction(video, canvas, model) {
   if (!model || !video || !canvas) return;
 
-  const predictions = await model.detect(video);
+  // Do Prediction with model
+  const input_tensor = process_input(video);
+  const predictions = await model.detect(input_tensor);
+  
+  function process_input(video_html) {
+    let processed_video = tf.browser.fromPixels(video_html);
+    processed_video = tf.image.resizeBilinear(processed_video, [320, 320]);
+    return processed_video;
+  }
 
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, video.videoWidth, video.videoHeight);
@@ -32,7 +46,7 @@ export async function predictionFunction(video, canvas, model) {
     for (let n = 0; n < predictions.length; n++) {
       // Check scores
       console.log(n);
-      if (predictions[n].score > 0.8) {
+      if (predictions[n].score > threshold) {
         let bboxLeft = predictions[n].bbox[0];
         let bboxTop = predictions[n].bbox[1];
         let bboxWidth = predictions[n].bbox[2] - bboxLeft; //dunno why I need to do this
